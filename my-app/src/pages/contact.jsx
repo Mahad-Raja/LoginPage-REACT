4bbc85dfdea196590ac2b959f70cca4e27bfc5fd
Mahad-Raja <mahadsajid16@gmail.com>
2026-07-21 10:18:07 +0500

    Create contact.jsx

---
+++ my-app/src/pages/contact.jsx
@@ -0,0 +1,17 @@
+function Contact(){
+
+    return(
+       <div className="flex justify-center items-center min-h-[80vh]">
+       <div className="bg-white p-10 rounded-xl shadow-lg w-[600px] text-center">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">
+          Contact
+        </h1>
+        Email: support@example.com
+        <br></br>
+        Phone: +92 XXX XXXXXXX
+        
+    </div>
+</div>
+    );
+}
+export default Contact;
